1b9c377afbc049c4e68f46e943532cdd9697c843
Yurim Jin <jayjinjay@gmail.com>
2021-08-13 22:19:20 +0900

    demo app TS기반으로 재세팅

---
+++ demo/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
