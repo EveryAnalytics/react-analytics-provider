dd961d197ea2796671aa309c55b1c6b9b2fc3334
greatSumini <greatSumini@gmail.com>
2021-08-16 23:13:09 +0900

    chore: add reference link to googleAnalytics.set comment

--- src/utils/googleAnalytics/set.ts
+++ src/utils/googleAnalytics/set.ts
@@ -3,6 +3,7 @@ import {UnknownRecord} from '../../types/common';
 import {gtag} from './initialize';
 
 /** Allows you to set values that persist across all the subsequent gtag() calls on the page.
+ * {@link https://developers.google.com/gtagjs/reference/api#set API Reference}
  * @param params key-value pairs that are to persist across gtag() calls. */
 export function set(params: UnknownRecord) {
   console.info(`✅GA: set`, params);
