3bc887e9e7e62f578511d01171d37fa5410d6c7c
greatSumini <greatSumini@gmail.com>
2021-08-16 22:07:08 +0900

    feat: add googleAnalytics.set

---
+++ src/utils/googleAnalytics/set.ts
@@ -0,0 +1,10 @@
+import {UnknownRecord} from '../../types/common';
+
+import {gtag} from './initialize';
+
+/** Allows you to set values that persist across all the subsequent gtag() calls on the page.
+ * @param params key-value pairs that are to persist across gtag() calls. */
+export function set(params: UnknownRecord) {
+  console.info(`✅GA: set`, params);
+  gtag('set', params);
+}
