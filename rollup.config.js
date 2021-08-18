b1cc3504dba3f76bdecaeacbc5f4fe584f35d2da
HyojinKim <gywls00100@gmail.com>
2021-08-18 13:22:34 +0900

    충돌 해결

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,31 @@
-import typescript from "rollup-plugin-typescript2";
-import commonjs from "rollup-plugin-commonjs";
-import external from "rollup-plugin-peer-deps-external";
-import resolve from "rollup-plugin-node-resolve";
-import serve from "rollup-plugin-serve";
-import pkg from "./package.json";
+import typescript from 'rollup-plugin-typescript2';
+import commonjs from 'rollup-plugin-commonjs';
+import external from 'rollup-plugin-peer-deps-external';
+import resolve from 'rollup-plugin-node-resolve';
+import serve from 'rollup-plugin-serve';
+import pkg from './package.json';
 
-const extensions = [".js", ".jsx", ".ts", ".tsx"]; // 어떤 확장자를 처리 할 지 정함
+const extensions = ['.js', '.jsx', '.ts', '.tsx']; // 어떤 확장자를 처리 할 지 정함
 
 export default {
-  input: "src/index.tsx",
+  input: 'src/index.tsx',
   output: [
     {
       file: pkg.module,
-      format: "es",
+      format: 'es',
     },
   ],
   plugins: [
     external(),
-    resolve({ extensions }),
+    resolve({extensions}),
     typescript({
       rollupCommonJSResolveHack: true,
-      exclude: "**/__tests__/**",
+      exclude: '**/__tests__/**',
       clean: true,
     }),
     commonjs({
-      include: ["node_modules/**"],
+      include: ['node_modules/**'],
     }),
-    serve("dist"),
+    serve('dist'),
   ],
 };
--- src/utils/googleAnalytics/index.ts
+++ src/utils/googleAnalytics/index.ts
@@ -1,9 +1,11 @@
 import {event} from './event';
 import {click} from './click';
 import {initialize} from './initialize';
+import {set} from './set';
 
 export const googleAnalytics = {
   initialize,
   event,
   click,
+  set,
 };
