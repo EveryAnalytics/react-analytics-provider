02ceaca93c3c5b3dcaa0185ed150f95997621a62
YoungChang Lee <apricotsoul@gmail.com>
2021-08-21 13:59:40 +0900

    style: lint fix

--- demo/with-nextjs/src/pages/index.tsx
+++ demo/with-nextjs/src/pages/index.tsx
@@ -1,11 +1,7 @@
-import type { NextPage } from 'next'
+import type {NextPage} from 'next';
 
 const Home: NextPage = () => {
-  return (
-    <div>
-     Next Level
-    </div>
-  )
-}
+  return <div>Next Level</div>;
+};
 
-export default Home
+export default Home;
