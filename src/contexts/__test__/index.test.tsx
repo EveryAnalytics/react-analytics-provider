96e58ffc7fdc9f01c35b595eea747dbbbc9a46f3
Seongcheol Jo <josworks27@gmail.com>
2021-08-22 19:44:15 +0900

    feat/89: Add useAnalyticsContext(WIP)
    * useAnalyticsContext 테스트 코드 작성중
    * relates to #89

---
+++ src/contexts/__test__/index.test.tsx
@@ -0,0 +1,18 @@
+// import {useAnalyticsContext} from '../useAnalyticsContext';
+// import AnalyticsProviderContext from '../AnalyticsProviderContext';
+//
+// test('NameProvider composes full name from first, last', () => {
+//   const foo = useAnalyticsContext()
+//
+//   const providerProps = {
+//     // first: 'Boba',
+//     // last: 'Fett',
+//   }
+//   customRender(
+//     <AnalyticsProviderContext.Consumer>
+//       {value => <span>Received: {value}</span>}
+//     </AnalyticsProviderContext.Consumer>,
+//     {providerProps},
+//   )
+//   expect(screen.getByText(/^Received:/).textContent).toBe('Received: Boba Fett')
+// })
