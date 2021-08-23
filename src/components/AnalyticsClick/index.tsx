fd0203cc22080153c12d3b3899063ac715a99cc6
Seongcheol Jo <josworks27@gmail.com>
2021-08-23 23:13:13 +0900

    feat/99: Update AnalyticsClick
    * 기존 버블링을 사용한 방식에서 cloneElement로 엘리먼트를 만들어 반환하는 방식으로 변경
    * relates to #99

--- src/components/AnalyticsClick/index.tsx
+++ src/components/AnalyticsClick/index.tsx
@@ -1,4 +1,4 @@
-import React, {FC, MutableRefObject, useCallback, useEffect, useRef} from 'react';
+import React, {FC, useCallback} from 'react';
 import {useAnalyticsContext} from '../../contexts';
 import {UnknownRecord} from '../../types/common';
 
@@ -8,36 +8,17 @@ export interface AnalyticsClickProps {
   params?: UnknownRecord;
 }
 
-const useClickInside = (
-  ref: MutableRefObject<HTMLDivElement | null>,
-  callback: (name: string, params?: UnknownRecord) => void,
-  name: string,
-  params?: UnknownRecord,
-) => {
-  const handleClick = useCallback(
-    e => {
-      if (ref.current && ref.current.contains(e.target)) {
-        callback(name, params);
-      }
-    },
-    [ref, name, params, callback],
-  );
-
-  useEffect(() => {
-    const rootNode = document.getElementById('root');
-
-    rootNode?.addEventListener('click', handleClick);
-    return () => {
-      rootNode?.removeEventListener('click', handleClick);
-    };
-  }, [handleClick]);
-};
-
-export const AnalyticsClick: FC = ({children, name, params}: AnalyticsClickProps) => {
-  const ref = useRef<HTMLDivElement | null>(null);
+export const AnalyticsClick: FC = (props: AnalyticsClickProps) => {
   const {onClick} = useAnalyticsContext();
+  const {children, name, params} = props;
 
-  useClickInside(ref, onClick, name, params);
+  const child = React.Children.only(children) as React.ReactElement;
 
-  return <div ref={ref}>{children}</div>;
+  const handleChildClick = useCallback(() => {
+    onClick(name, {action_type: 'click', ...params});
+  }, [name, params, onClick]);
+
+  return React.cloneElement(child, {
+    onClick: handleChildClick,
+  });
 };
