b6f12719b173213e762fe2865940a8af8e62f559
MiryangJung <miryang.dev@gmail.com>
2021-08-29 19:42:07 +0900

    feat/151 setting set test

---
+++ tests/utils/googleAnalytics/set.test.ts
@@ -0,0 +1,10 @@
+import * as faker from 'faker';
+
+import * as initUtils from '../../../src/utils/googleAnalytics/initialize';
+import {set} from '../../../src/utils/googleAnalytics/set';
+
+describe('googleAnalytics.set', () => {
+  const setUp = () => {
+    const name = faker.lorem.word();
+  };
+});
