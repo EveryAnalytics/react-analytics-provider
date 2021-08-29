4831e5f7dcb06cf6a0a337767ba66b4a71651624
정대윤 <sensecodevalue@gmail.com>
2021-08-29 18:00:51 +0900

    test: googleAnalytics/event의 test code 추가 test 네이밍 및 변수 다른 test와 맞춤

---
+++ tests/utils/googleAnalytics/event.test.ts
@@ -0,0 +1,46 @@
+import * as faker from 'faker';
+
+import * as initUtils from '../../../src/utils/googleAnalytics/initialize';
+import { event } from '../../../src/utils/googleAnalytics/event';
+
+describe('googleAnalytics.initialize', () => {
+  const setUp = () => {
+    const trackingId = faker.lorem.word();
+    initUtils.initialize(trackingId);
+
+    const mockDate = faker.datatype.datetime();
+    jest.useFakeTimers();
+    jest.setSystemTime(mockDate);
+
+    const name = faker.lorem.word();
+    const params = {foo: 'bar'};
+
+    const gtagSpy = jest.spyOn(initUtils, 'gtag');
+    const consoleInfoSpy = jest.spyOn(global.console, 'info');
+
+    return {
+      name,
+      params,
+      gtagSpy,
+      consoleInfoSpy,
+    };
+  };
+
+  test('should record a event with fake name', () => {
+    const {name, gtagSpy, consoleInfoSpy} = setUp();
+
+    event(name);
+
+    expect(gtagSpy).toHaveBeenNthCalledWith(1, 'event', name, undefined);
+    expect(consoleInfoSpy).toHaveBeenCalledTimes(1);
+  });
+  
+  test('should record a event with fake name and params', () => {
+    const {name, params, gtagSpy, consoleInfoSpy} = setUp();
+
+    event(name, params);
+    
+    expect(gtagSpy).toHaveBeenNthCalledWith(1, 'event', name, params);
+    expect(consoleInfoSpy).toHaveBeenCalledTimes(1);
+  });
+});
