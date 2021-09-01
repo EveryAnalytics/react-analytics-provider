fe73d352a6ad253801b23a29d2718fcab51d0ecb
MiryangJung <miryang.dev@gmail.com>
2021-09-01 20:01:56 +0900

    test: googleAnalytics.set 테스트 추가

--- tests/utils/googleAnalytics/set.test.ts
+++ tests/utils/googleAnalytics/set.test.ts
@@ -1,10 +1,26 @@
-import * as faker from 'faker';
-
 import * as initUtils from '../../../src/utils/googleAnalytics/initialize';
 import {set} from '../../../src/utils/googleAnalytics/set';
 
 describe('googleAnalytics.set', () => {
   const setUp = () => {
-    const name = faker.lorem.word();
+    const params = {foo: 'bar'};
+
+    const gtagSpy = jest.spyOn(initUtils, 'gtag').mockImplementation(() => null);
+    const consoleInfoSpy = jest.spyOn(global.console, 'info');
+
+    return {
+      params,
+      gtagSpy,
+      consoleInfoSpy,
+    };
   };
+
+  test('should record a set with params', () => {
+    const {params, gtagSpy, consoleInfoSpy} = setUp();
+
+    set(params);
+
+    expect(gtagSpy).toHaveBeenCalledWith('set', params);
+    expect(consoleInfoSpy).toHaveBeenCalledTimes(1);
+  });
 });
