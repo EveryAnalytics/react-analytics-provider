45714b5385a3dd60bf9431350c21c493fb9cb00f
MiryangJung <miryang.dev@gmail.com>
2021-09-01 20:45:42 +0900

    test: 변경된 set 모듈에 맞춰 변경

--- tests/utils/googleAnalytics/set.test.ts
+++ tests/utils/googleAnalytics/set.test.ts
@@ -1,14 +1,18 @@
+import * as faker from 'faker';
+
 import * as initUtils from '../../../src/utils/googleAnalytics/initialize';
 import {set} from '../../../src/utils/googleAnalytics/set';
 
 describe('googleAnalytics.set', () => {
   const setUp = () => {
+    const name = faker.lorem.word();
     const params = {foo: 'bar'};
 
     const gtagSpy = jest.spyOn(initUtils, 'gtag').mockImplementation(() => null);
     const consoleInfoSpy = jest.spyOn(global.console, 'info');
 
     return {
+      name,
       params,
       gtagSpy,
       consoleInfoSpy,
@@ -21,6 +25,15 @@ describe('googleAnalytics.set', () => {
     set(params);
 
     expect(gtagSpy).toHaveBeenCalledWith('set', params);
+    expect(consoleInfoSpy).toHaveBeenCalledTimes(1);
+  });
+
+  test('should record a set with fake name and paramss', () => {
+    const {name, params, gtagSpy, consoleInfoSpy} = setUp();
+
+    set(name, params);
+
+    expect(gtagSpy).toHaveBeenCalledWith('set', name, params);
     expect(consoleInfoSpy).toHaveBeenCalledTimes(1);
   });
 });
