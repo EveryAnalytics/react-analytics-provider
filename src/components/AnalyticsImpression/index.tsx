74d19b92045f8346dec6a2b2a5ad5d5e1cdc5809
mumrik <uoon.dev@gmail.com>
2021-09-03 00:31:54 +0900

    feat: add observer to AnalyticsImpression

--- src/components/AnalyticsImpression/index.tsx
+++ src/components/AnalyticsImpression/index.tsx
@@ -1,20 +1,30 @@
 import React from 'react';
 import {useAnalyticsContext} from '../../contexts/useAnalyticsContext';
 import {UnknownRecord} from '../../types/common';
+import {useOnScreen, useOnScreenProps} from '../../hooks';
 
-export interface AnalyticsImpressionProps {
+export interface AnalyticsImpressionProps extends useOnScreenProps {
   children: React.ReactNode;
   name: string;
   // TODO: modify params type
   params: UnknownRecord;
 }
 
-export const AnalyticsImpression = ({children, name, params}: AnalyticsImpressionProps) => {
+export const AnalyticsImpression = ({
+  children,
+  name,
+  params,
+  rootMargin,
+  threshold,
+  triggerOnce,
+  delay,
+}: AnalyticsImpressionProps) => {
   const analytics = useAnalyticsContext();
+  const [ref, isIntersecting] = useOnScreen<HTMLHeadingElement>({rootMargin, threshold, triggerOnce, delay});
 
   React.useEffect(() => {
-    analytics.onImpression(name, params);
-  }, [analytics, name, params]);
+    if (isIntersecting) analytics.onImpression(name, params);
+  }, [analytics, name, params, isIntersecting]);
 
-  return <>{children}</>;
+  return <div ref={ref}>{children}</div>;
 };
