3f52854dd8d6b99337b4f54d27b75d9169b50c22
정대윤 <sensecodevalue@gmail.com>
2021-09-05 19:17:35 +0900

    refactor: 빈 사용자 정보 객체인지를 변수로 관리하기

--- src/utils/amplitude/setUserProperties.ts
+++ src/utils/amplitude/setUserProperties.ts
@@ -3,9 +3,11 @@ import {UnknownRecord} from '../../types/common';
 
 export const setUserProperties = (userProperties: UnknownRecord) => {
   // eslint-disable-next-line import/no-named-as-default-member
-  if(Object.entries(userProperties).length == 0) {
+  const isEmptyUserProperties = Object.entries(userProperties).length == 0;
+  if(isEmptyUserProperties) {
       console.warn('[Warning] Amplitude setUserProperties: userProperties is empty Object. Please fill in the user information.')
       return;
   }
   return amplitude.getInstance().setUserProperties(userProperties);
 };
+ 
