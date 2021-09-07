2cc3a4c5a32d8f5b82704a1c08d7fab66af355a4
정대윤 <sensecodevalue@gmail.com>
2021-09-08 01:29:59 +0900

    delete: lint ignore which have not need

--- tests/utils/amplitude/setUserProperties.test.ts
+++ tests/utils/amplitude/setUserProperties.test.ts
@@ -11,7 +11,6 @@ describe('amplitudeHelper.setUserProperties', () => {
       () =>
         ({
           setUserProperties: amplitudeSetUserPropertiesMock,
-          // eslint-disable-next-line @typescript-eslint/no-explicit-any
         } as any),
     );
 
