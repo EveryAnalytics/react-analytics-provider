1c3ceb0f6596f202a957d2e7cd0be6ecea7dc959
정대윤 <sensecodevalue@gmail.com>
2021-10-03 01:45:44 +0900

    feat: Add types is used in ecommerce event

---
+++ src/types/ecommerce.ts
@@ -0,0 +1,36 @@
+export type Impression = {
+  id: string;
+  name: string;
+  list_name?: string;
+  brand?: string;
+  category?: string;
+  variant?: string;
+  list_position?: number;
+  price?: number;
+};
+
+export type Product = {
+  id: string;
+  name: string;
+  list_name?: string;
+  brand?: string;
+  category?: string;
+  variant?: string;
+  price?: number;
+  quantity?: number;
+  coupon?: string;
+  list_position?: number;
+};
+
+export type Action = {
+  items?: Array<Product>;
+};
+
+export type Transaction = {
+  transaction_id: string;
+  affiliation?: string;
+  value?: number;
+  tax?: number;
+  shipping?: number;
+  items?: Array<Product>;
+};
