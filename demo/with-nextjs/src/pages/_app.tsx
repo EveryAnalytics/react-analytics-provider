2b6a77e74cf52dddf559eaa5d507f838c713df46
Yurim Jin <jayjinjay@gmail.com>
2021-10-10 19:17:59 +0900

    Remove ignoring /demo in eslint to lint check entire app

--- demo/with-nextjs/src/pages/_app.tsx
+++ demo/with-nextjs/src/pages/_app.tsx
@@ -1,6 +1,4 @@
-import { amplitudeHelper } from '@every-analytics/react-analytics-provider';
-import { googleAnalyticsHelper } from '@every-analytics/react-analytics-provider';
-import {AnalyticsProvider} from '@every-analytics/react-analytics-provider';
+import {amplitudeHelper, googleAnalyticsHelper, AnalyticsProvider} from '@every-analytics/react-analytics-provider';
 import {AppProps} from 'next/app';
 import {Layout} from '../layout/Layout';
 
@@ -10,10 +8,12 @@ function DemoApp({Component, pageProps}: AppProps) {
   return (
     <AnalyticsProvider
       onInitialize={() => {
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
         googleAnalyticsHelper.initialize(process.env.NEXT_PUBLIC_AMPLITUDE_API_KEY!, persistentValues);
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
         amplitudeHelper.initialize(process.env.NEXT_PUBLIC_AMPLITUDE_API_KEY!);
       }}
-      onPageView={params => {
+      onPageView={() => {
         // NOTE: Google Analytics(GA4)는 기본적으로 페이지뷰가 적용됩니다 - 따로 추가 필요X
         const path = window.location.pathname + window.location.search;
         amplitudeHelper.logEvent('pageView', {path});
