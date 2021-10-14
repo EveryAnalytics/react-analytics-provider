0af71fc82f75f2c49f02fd770a3942876fa32f31
greatSumini <greatSumini@gmail.com>
2021-10-15 04:38:19 +0900

    chore: add sample (with storybook)

---
+++ src/react/sample.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Analytics} from '../core';
+
+Analytics.preset({
+    init: async () => {console.info('init completed')},
+    event: (...args) => {console.info('event occured! ' + args)}
+})
+
+const Sample = () => {
+  return <button onClick={() => { Analytics.event('storybook click') }}>click me</button>;
+};
+
+export default {
+  title: 'Sample',
+  component: Sample,
+};
+
+export const Default = () => <Sample />;
