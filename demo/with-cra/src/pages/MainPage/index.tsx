6b3b147290c007d64b0323a709551b37b980a880
Leehaeun0 <a01050675111@gmail.com>
2021-10-26 10:08:57 +0900

    feat: apply LayoutWithNav component to pages

--- demo/with-cra/src/pages/MainPage/index.tsx
+++ demo/with-cra/src/pages/MainPage/index.tsx
@@ -1,14 +1,16 @@
 import {useAnalyticsPageView} from '@every-analytics/react-analytics-provider';
+import LayoutWithNav from '../../components/LayoutWithNav';
 import {getQueryParams} from '../../utils/location';
 
 const MainPage = () => {
   const {referrer} = getQueryParams<{referrer: string}>();
   useAnalyticsPageView({referrer});
 
+  // Todo: Remove the LayoutWithNav after the main page is complete
   return (
-    <>
+    <LayoutWithNav>
       <h2>Fruit Store</h2>
-    </>
+    </LayoutWithNav>
   );
 };
 
