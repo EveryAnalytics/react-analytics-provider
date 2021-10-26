88896a04b3730cd874b53aff1799eac406a9f905
Leehaeun0 <a01050675111@gmail.com>
2021-10-26 12:03:04 +0900

    remove: remove index.css

--- demo/with-cra/src/index.tsx
+++ demo/with-cra/src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import {AnalyticsProvider, googleAnalyticsHelper, amplitudeHelper} from '@every-analytics/react-analytics-provider';
 import {fruitLogger} from './utils/fruitLogger';
