819473345ff62135edd1f978e7adf0f0e7ee3e58
Leehaeun0 <a01050675111@gmail.com>
2021-10-26 10:04:44 +0900

    feat: update Navbar
    * add active to button
    
    * change layout with wrapper and children
    
    * update styles

--- demo/with-cra/src/components/NavBar.tsx
+++ demo/with-cra/src/components/NavBar.tsx
@@ -2,56 +2,60 @@ import React from 'react';
 import {useAnalyticsContext} from '@every-analytics/react-analytics-provider';
 import navigate from '../router/navigate';
 import styled from '@emotion/styled';
+import {css} from '@emotion/react';
 
-export default function NavBar() {
+export default function Navbar({children}: {children: React.ReactNode}) {
   const analytics = useAnalyticsContext();
 
   return (
-    <Nav>
-      <Ul>
-        <NavItem
-          href="/"
-          onClick={() => {
-            analytics.onEvent('Click logo');
-          }}
-        >
-          Fruit Store
-        </NavItem>
-        <NavItem
-          href="/products?color=red"
-          onClick={() => {
-            analytics.onEvent('Click products', {color: 'red'});
-          }}
-        >
-          Red
-        </NavItem>
-        <NavItem
-          href="/products?color=yellow"
-          onClick={() => {
-            analytics.onEvent('Click products', {color: 'yellow'});
-          }}
-        >
-          Yellow
-        </NavItem>
-        <NavItem
-          href="/login"
-          onClick={() => {
-            analytics.onClick('Click login', {color: 'yellow'});
-          }}
-        >
-          Login
-        </NavItem>
-        <NavItem
-          href="/set-currency"
-          onClick={() => {
-            analytics.onSet({currency: 'KRW'});
-          }}
-        >
-          Currency
-        </NavItem>
-        <NavItem href="/set-user-property">UserProperty</NavItem>
-      </Ul>
-    </Nav>
+    <Wrapper>
+      <Nav>
+        <Ul>
+          <NavItem
+            href="/"
+            onClick={() => {
+              analytics.onEvent('Click logo');
+            }}
+          >
+            Fruit Store
+          </NavItem>
+          <NavItem
+            href="/products?color=red"
+            onClick={() => {
+              analytics.onEvent('Click products', {color: 'red'});
+            }}
+          >
+            Red
+          </NavItem>
+          <NavItem
+            href="/products?color=yellow"
+            onClick={() => {
+              analytics.onEvent('Click products', {color: 'yellow'});
+            }}
+          >
+            Yellow
+          </NavItem>
+          <NavItem
+            href="/login"
+            onClick={() => {
+              analytics.onClick('Click login', {color: 'yellow'});
+            }}
+          >
+            Login
+          </NavItem>
+          <NavItem
+            href="/set-currency"
+            onClick={() => {
+              analytics.onSet({currency: 'KRW'});
+            }}
+          >
+            Currency
+          </NavItem>
+          <NavItem href="/set-user-property">UserProperty</NavItem>
+        </Ul>
+      </Nav>
+      <Content>{children}</Content>
+    </Wrapper>
   );
 }
 
@@ -68,18 +72,25 @@ const NavItem = ({
     navigate.push(href);
     onClick?.(e); // NOTE: prettier에서 에러로 인식하네?
   };
+
+  const active = href === window.location.pathname + window.location.search;
+
   return (
     <Li>
-      <NavItemButton onClick={handleClick}>{children}</NavItemButton>
+      <NavItemButton onClick={handleClick} active={active}>
+        {children}
+      </NavItemButton>
     </Li>
   );
 };
 
-const Nav = styled.nav`
-  display: inline-block;
+const Wrapper = styled.div`
+  display: flex;
+  height: calc(100vh - 6rem);
+`;
+
+const Nav = styled.aside`
   width: 300px;
-  height: 100%;
-  padding-top: 4rem;
   border-right: 1px solid #ebebeb;
 `;
 
@@ -91,21 +102,27 @@ const Ul = styled.ul`
 
 const Li = styled.li``;
 
-const NavItemButton = styled.button`
+const NavItemButton = styled.button<{active: boolean}>`
   text-align: left;
   width: 100%;
   height: 50px;
   padding: 0 24px;
-  border: none;
-  background-color: transparent;
-  font-size: 1rem;
+  font-size: 1.6rem;
   font-weight: 600;
-  cursor: pointer;
-  &:hover {
-    background-color: #f7f7f7;
-  }
+
+  &:hover,
   &:focus {
-    color: #0095f6;
-    background-color: #f7f7f7;
+    background-color: #fbfbfb;
   }
+
+  ${props =>
+    props.active &&
+    css`
+      color: #0095f6;
+      background-color: #f7f7f7 !important;
+    `}
+`;
+
+const Content = styled.div`
+  flex: 1;
 `;
