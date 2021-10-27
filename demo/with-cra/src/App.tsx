08827f4a70dcb08a719c7ffc19edd21a04d7190a
Leehaeun0 <a01050675111@gmail.com>
2021-10-27 10:37:19 +0900

    remove: remove CurrencyPage, UserPropertyPage
    * remove CurrencyPage, UserPropertyPage directory
    
    * remove CurrencyPage, UserPropertyPage router

--- demo/with-cra/src/App.tsx
+++ demo/with-cra/src/App.tsx
@@ -1,8 +1,6 @@
 import MainPage from './pages/MainPage';
 import ProductsPage from './pages/ProductsPage';
 import LoginPage from './pages/LoginPage';
-import CurrencyPage from './pages/CurrencyPage';
-import UserPropertyPage from './pages/UserPropertyPage';
 import Route from './router/Route';
 import Main from './components/Main';
 import Header from './components/Header';
@@ -20,12 +18,6 @@ function App() {
         </Route>
         <Route path="/login">
           <LoginPage />
-        </Route>
-        <Route path="/set-currency">
-          <CurrencyPage />
-        </Route>
-        <Route path="/set-user-property">
-          <UserPropertyPage />
         </Route>
       </Main>
     </>
