3c1c08c719df27f9984c623abcd61141e511b185
정대윤 <sensecodevalue@gmail.com>
2021-10-30 03:07:09 +0900

    rebase: merge with main

--- demo/with-cra/src/App.tsx
+++ demo/with-cra/src/App.tsx
@@ -1,12 +1,14 @@
 import MainPage from './pages/MainPage';
 import ProductsPage from './pages/ProductsPage';
 import LoginPage from './pages/LoginPage';
+import CategoryPage from './pages/CategoryPage';
 import Route from './router/Route';
 import Main from './components/Main';
 import Header from './components/Header';
 
 function App() {
   return (
+<<<<<<< HEAD
     <>
       <Header />
       <Main>
@@ -18,6 +20,9 @@ function App() {
         </Route>
         <Route path="/login">
           <LoginPage />
+        </Route>
+        <Route path="/category">
+          <CategoryPage />
         </Route>
       </Main>
     </>
