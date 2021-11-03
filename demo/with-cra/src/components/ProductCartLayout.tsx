c7935c068699ee15c1d58974209b7207b7627eed
HyojinKim <gywls00100@gmail.com>
2021-11-03 16:06:35 +0900

    Add ProductCartLayout

---
+++ demo/with-cra/src/components/ProductCartLayout.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+function ProductCartLayout({children}: {children: React.ReactNode}) {
+  return <div>{children}</div>;
+}
+
+export default ProductCartLayout;
