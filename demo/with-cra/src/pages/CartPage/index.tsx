243aa11938673e251f5a227167ba6344e5fe2670
HyojinKim <gywls00100@gmail.com>
2021-11-03 18:42:33 +0900

    Add router into 'buy now' button

--- demo/with-cra/src/pages/CartPage/index.tsx
+++ demo/with-cra/src/pages/CartPage/index.tsx
@@ -1,6 +1,7 @@
 import ProductCartItems from '../../components/ProductCartItems';
 import ProductCartLayout from '../../components/ProductCartLayout';
 import {getQueryParams} from '../../utils/location';
+import navigate from '../../router/navigate';
 import products from '../../mocks/ecommerce/products.json';
 import styled from '@emotion/styled';
 import {useAnalyticsPageView} from '@every-analytics/react-analytics-provider';
@@ -9,8 +10,12 @@ const CartPage = () => {
   const {referrer} = getQueryParams<{referrer: string}>();
   useAnalyticsPageView({referrer});
 
+  const onClick = () => {
+    navigate.push('/purchase'); //TODO: 구매페이지 router 이름에 맞게 수정하기
+  };
+
   return (
-    <ProductCartLayout title="My cart list">
+    <ProductCartLayout title="My cart list" buttonContent="Buy Now" onClick={onClick}>
       <ProductCartItems items={products} />
     </ProductCartLayout>
   );
