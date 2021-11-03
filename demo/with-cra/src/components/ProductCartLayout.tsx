6bc3915e25ff9641fb118903951514b1cec97312
HyojinKim <gywls00100@gmail.com>
2021-11-03 17:44:56 +0900

    Add title props

--- demo/with-cra/src/components/ProductCartLayout.tsx
+++ demo/with-cra/src/components/ProductCartLayout.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
-function ProductCartLayout({children}: {children: React.ReactNode}) {
-  return <div>{children}</div>;
+function ProductCartLayout({title, children}: {title: string; children: React.ReactNode}) {
+  return (
+    <LayoutSpacing>
+      <Title>{title}</Title>
+      <ContentSpacing>{children}</ContentSpacing>
+    </LayoutSpacing>
+  );
 }
 
 export default ProductCartLayout;
+
+const LayoutSpacing = styled.div`
+  margin: 50px;
+`;
+
+const ContentSpacing = styled.div`
+  margin: 50px;
+`;
+
+const Title = styled.h1`
+  font-size: 30px;
+  font-weight: bold;
+`;
