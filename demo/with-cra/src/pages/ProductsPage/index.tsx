140e2cc9ba96d261e2c9a25dad9a6adbaf597d62
SenseCodeValue <sensecodevalue@gmail.com>
2021-11-07 15:28:28 +0900

    feat: Add style and mapping products data to mocks data in ProductPage.

--- demo/with-cra/src/pages/ProductsPage/index.tsx
+++ demo/with-cra/src/pages/ProductsPage/index.tsx
@@ -1,38 +1,56 @@
 import {useEffect} from 'react';
 import {getQueryParams} from '../../utils/location';
 import {useAnalyticsContext} from '@every-analytics/react-analytics-provider';
+
+import navigate from '../../router/navigate';
+import Products from '../../mocks/ecommerce/products.json';
+import {ProductType} from '../../types/Product';
+
 import ProductNav from '../../components/ProductNav';
+import Cards from '../../components/Cards';
+import Card from '../../components/Card';
 
 const ProductsPage = () => {
   const {color} = getQueryParams<{color: string}>();
-  const products = getProductsByColor(color);
+  const products: ProductType[] = getProductsByColor(color);
   const analytics = useAnalyticsContext();
 
   useEffect(() => {
     analytics.onPageView();
   }, [analytics]);
 
+  const getProductDetailUrl = (product: ProductType): string => {
+    return `/product?color=${color}&product=${product.name.en}`;
+  };
+
   return (
     <ProductNav>
       <h2>{color} fruits</h2>
-      <ul>
-        {products.map(product => (
-          <li key={product}>{product}</li>
+      <Cards>
+        {products.map((product: ProductType) => (
+          <Card
+            title={product.name.en}
+            onClick={() => {
+              navigate.push(getProductDetailUrl(product));
+            }}
+          />
         ))}
-      </ul>
+      </Cards>
     </ProductNav>
   );
 };
 
 export default ProductsPage;
 
-function getProductsByColor(color: string) {
+function getProductsByColor(color: string): ProductType[] {
   switch (color) {
     case 'red':
-      return ['Strawberry', 'Tomato'];
+      return Products.filter((product: ProductType) => product.categoryId === 'RED');
     case 'yellow':
-      return ['Banana', 'Lemon'];
+      return Products.filter((product: ProductType) => product.categoryId === 'YELLOW');
+    case 'green':
+      return Products.filter((product: ProductType) => product.categoryId === 'GREEN');
     default:
-      return [];
+      return Products;
   }
 }
